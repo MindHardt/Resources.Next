a9c545eb5e0ddf8fbfe3b5bd2d74f751a6a766ce
Игорь Бабин <igorbabin2002@gmail.com>
2024-02-11 03:17:20 +0500

    Removed examples and redundant setter for DefaultLocale

--- Resources.Next.Generator/Resource.cs
+++ Resources.Next.Generator/Resource.cs
@@ -8,11 +8,7 @@ internal record Resource(string Name, Dictionary<string, string> Locales)
     public string Name { get; } = Name;
     public Dictionary<string, string> Locales { get; } = Locales;
 
-    public string DefaultLocale
-    {
-        get => Locales[string.Empty];
-        set => Locales[string.Empty] = value;
-    }
+    public string DefaultLocale => Locales[string.Empty];
 
     public IEnumerable<KeyValuePair<string, string>> OtherLocales
         => Locales.Where(x => x.Key != string.Empty);
